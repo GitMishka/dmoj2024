95c1cb182836ff75af3a99b3fe1ca34c04578534
Misha <mishagrigoryan@gmail.com>
2024-03-05 23:58:44 -0500

    works

--- Special Day.py
+++ Special Day.py
@@ -1,8 +1,13 @@
 n = [int(input()) for _ in range(2)]
 
-if n[0] < 2 and n[1] < 18:
-    print('Before')
-elif n[0] > 2 and n[1] > 18:
+if n[0] == 2 :
+    if n[1] < 18:
+        print('Before')
+    elif n[1] > 18:
+        print('After')
+    else:
+        print('Special')
+elif n[0] > 2 :
     print("After")
 else:
-    print("Special")
+    print("Before")
