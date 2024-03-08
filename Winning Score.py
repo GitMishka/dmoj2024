38260ecb196684d6d2db0463533cf5ecb23900c8
Misha <mishagrigoryan@gmail.com>
2024-03-07 23:49:37 -0500

    solved

--- Winning Score.py
+++ Winning Score.py
@@ -1,7 +1,31 @@
-n = [int(input) for i in range(6)]
+total_points_apples = 0
+total_points_bananas = 0
 
+points_for_3_shot = 3
+points_for_2_goal = 2
+points_for_1_free_throw = 1
 
-apples = 0
-bananas = 0
-for i in n in range(n[0:2]):
-    
+for _ in range(3):
+    score = int(input())
+    if _ == 0:  
+        total_points_apples += score * points_for_3_shot
+    elif _ == 1: 
+        total_points_apples += score * points_for_2_goal
+    else: 
+        total_points_apples += score * points_for_1_free_throw
+
+for _ in range(3):
+    score = int(input())
+    if _ == 0:  
+        total_points_bananas += score * points_for_3_shot
+    elif _ == 1:  
+        total_points_bananas += score * points_for_2_goal
+    else:  
+        total_points_bananas += score * points_for_1_free_throw
+
+if total_points_apples > total_points_bananas:
+    print("A")
+elif total_points_bananas > total_points_apples:
+    print("B")
+else:
+    print("T")
